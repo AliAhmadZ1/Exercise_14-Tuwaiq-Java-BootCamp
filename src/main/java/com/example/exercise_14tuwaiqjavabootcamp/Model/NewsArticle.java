4a1cff46b609c3f5dfab47478403e44df3236ec1
aliali <aliali@test.com>
2025-03-12 14:26:11 +0300

    finish Validation

--- src/main/java/com/example/exercise_14tuwaiqjavabootcamp/Model/NewsArticle.java
+++ src/main/java/com/example/exercise_14tuwaiqjavabootcamp/Model/NewsArticle.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -16,22 +17,22 @@ public class NewsArticle {
     @NotEmpty(message = "cannot be empty")
     private String id;
     @NotEmpty(message = "cannot be empty")
-    @Size(max = 100,message = "should be up to 100 characters...")
+    @Size(min = 4, max = 100,message = "title should be up to 100 characters...")
     private String title;
     @NotEmpty(message = "cannot be empty")
-    @Size(min = 4,max = 20,message = "must be more than 4 characters to 20 characters.")
+    @Size(min = 4,max = 20,message = "author SHOULD be more than 4 characters to 20 characters.")
     private String author;
     @NotEmpty(message = "cannot empty")
-    @Size(min = 200,message = "should be at least 200 characters")
+    @Size(min = 200,message = "CONTENT should be at least 200 characters")
     private String content;
     @NotEmpty(message = "cannot be empty")
-    @Pattern(regexp = "^(politics|sports|technology)$")
+    @Pattern(regexp = "^(politics|sports|technology)$",message = "category should be politics, sports or technology")
     private String category;
     @NotEmpty(message = "cannot empty")
     private String imageUrl;
-    @AssertFalse
+    @AssertFalse(message = "isPublished should be false")
     private boolean isPublished;
-
+    @DateTimeFormat(pattern = "yyyy/MM/dd")
     private LocalDate publishDate;
     //NewsArticle Model has:
         //▪ id:
